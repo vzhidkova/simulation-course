6a382beca16d0aa994bd5e919bfb774da862bf41
vzhidkova <vzidkova73@gmail.com>
2026-03-27 09:09:06 +0700

    Update sim_4.py

--- lab04/sim_4.py
+++ lab04/sim_4.py
@@ -2,21 +2,21 @@ import math
 import numpy as np
 
 
-x0 = 0.4
-m = 1.0
-a = 5.6
+x0 = 2**32 + 3 
+m = 2**63
+a = 2**32 + 3 
 N = 10 ** 5
 
 my_data = []
-my_data.append(x0)
-library_data = np.random.uniform(0.0, m, N)
-M1 = x0
-M2 = 0
+my_data.append(x0/m)
+library_data = np.random.uniform(0.0, 1, N)
+M1 = x0/m
+M2 = library_data[N-1]
 
-for i in range(N):
+for i in range(N-1):
     x0 = (x0 * a) % m
-    my_data.append(x0)
-    M1 += x0
+    my_data.append(x0/m)
+    M1 += x0/m
     M2 += library_data[i]
 
 M1 = M1/N
@@ -30,7 +30,7 @@ D1 = D1/N
 D2 = D2/N
 
 
-print("Теоретические результаты:", m/2, m**2/12)
+print("Теоретические результаты:                ", 1/2, 1**2/12)
 print("Мультипликативный конгруэнтный генератор:", M1, D1)
-print("Встроенный генератор:", M2, D2)
+print("Встроенный генератор:                    ", M2, D2)
 
